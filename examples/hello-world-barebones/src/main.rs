2a8fcbae4db268f906d9cb573c72debafc7572e3
GrayJack <gr41.j4ck@gmail.com>
2026-05-06 14:44:34 -0300

    docs: Add examples

---
+++ examples/hello-world-barebones/src/main.rs
@@ -0,0 +1,52 @@
+#![no_std]
+#![no_main]
+
+use core::ffi::c_void;
+
+use pspsdk::sys::{
+    thread::{sceKernelCreateThread, sceKernelStartThread, ThreadAttributes},
+    SceResult,
+};
+
+pspsdk::module_info!("sample_module", 1, 1);
+
+#[unsafe(no_mangle)]
+extern "C" fn module_start(argc_bytes: usize, argv: *mut c_void) -> isize {
+    pspsdk::println!("Hello from module start!");
+
+    // Set OS functions for pspsdk::io module
+    pspsdk::set_psp_os_functions();
+
+    unsafe {
+        let Ok(id) = sceKernelCreateThread(
+            c"main_thread".as_ptr().cast(),
+            psp_main_thread,
+            32,
+            256 * 1024,
+            ThreadAttributes::UserMode | ThreadAttributes::UseVFPU,
+            None,
+        )
+        .into_result() else {
+            return -1;
+        };
+
+        let _res = sceKernelStartThread(id, argc_bytes, argv);
+    }
+    0
+}
+
+extern "C" fn psp_main_thread(_argc: usize, _argv: *mut c_void) -> SceResult<u32> {
+    pspsdk::enable_home_button();
+
+    pspsdk::dprintln!("Hello PSP from rust!");
+    pspsdk::println!("Hello PSP from rust!");
+
+    SceResult::new(0)
+}
+
+pspsdk::exports! {
+    "syslib", 0, 2, 0x8000, [
+        fn module_start,
+        static module_info.0 : 0xF01D73A7,
+    ];
+}
