e501cd7f79bdb6af9f8762230c9aace40a1b2b97
GrayJack <gr41.j4ck@gmail.com>
2026-09-13 04:24:56 -0300

    refactor(cargo-pspbuild): Update minimum rustc version

--- cargo-pspbuild/src/main.rs
+++ cargo-pspbuild/src/main.rs
@@ -196,13 +196,13 @@ impl core::ops::Add<CommitDate> for CommitDate {
 // below. Note that the `day` field lags by one day, as the toolchain always
 // contains the previous days' nightly rustc.
 const MINIMUM_COMMIT_DATE: CommitDate = CommitDate {
-    year: 2025,
-    month: 3,
-    day: 18,
+    year: 2026,
+    month: 9,
+    day: 6,
 };
 const MINIMUM_RUSTC_VERSION: Version = Version {
     major: 1,
-    minor: 87,
+    minor: 100,
     patch: 0,
     pre: Prerelease::EMPTY,
     build: BuildMetadata::EMPTY,
